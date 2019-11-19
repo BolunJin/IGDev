621514eeb1677be9e4680e27b9dc604748b32efb
BolunJin <jbl9566wow@gmail.com>
2019-11-18 21:01:24 -0500

    First Post Image

--- IG/admin.py
+++ IG/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from IG.models import Post
 
 # Register your models here.
+# add apps which are displayed in admin page
+admin.site.register(Post) # Regist model "Post" in app "admin"
--- IG/models.py
+++ IG/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
 
 # Create your models here.
+
+# Create a model for posting image and alternative title
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        # storage address when usr upload an image
+        upload_to='static/images/posts',
+        # image format
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
